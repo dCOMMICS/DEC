907ca8eb30a1f502c4c7b65101ab82074fa67dd8
dCOMMICS <sanwurld@gmail.com>
2026-06-09 21:08:16 +0300

    adding python file

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 # the excel file importation
 
-df = pd.read_excel ('/big_flight_operations_dirty_1M.csv')
+df = pd.read_csv('/big_flight_operations_dirty_1M.csv', low_memory= False)
 
+# 
 
 print(df)
