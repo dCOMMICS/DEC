19062eb932d82523de0ecf520df5c8ee2ea998dc
dCOMMICS <sanwurld@gmail.com>
2026-06-09 20:05:52 +0300

    fixing error from previous files

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# the excel file importation
+
+# df = pd.read_excel ('/big_flight_operations_dirty_1M.csv')
+
+
+path _to_file = " C:\Users\ADMIN\Documents\DEC\big_flight_operations_dirty_1M.csv"
+airport_data = pd.read_csv(path_to_file)
+
+print(df)
